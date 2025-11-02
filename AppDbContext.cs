8dafa7160ab1f5e5415b282b195fefbe4e25c895
Harshit Sachdeva <119051777+Harshitron@users.noreply.github.com>
2025-11-02 17:45:33 +0530

    Db Context file

---
+++ AppDbContext.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using author_book.Models;
+
+namespace author_book
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=AuthorBookDb;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure the one-to-many relationship between Author and Book
+            modelBuilder.Entity<Author>()
+                .HasMany(a => a.Books)
+                .WithOne(b => b.Author)
+                .HasForeignKey(b => b.AuthorId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
